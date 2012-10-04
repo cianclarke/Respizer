2559bb3659a4fb357ca2038c502e55ed44fbd5df
cianclarke <cian@cianclarke.com>
2012-10-04 21:01:15 +0100

    Add tests

---
+++ tests.js
@@ -0,0 +1,16 @@
+var Respizer = require('./lib/respizer.js'),
+assert = require('assert'),
+fs = require('fs');
+
+console.log('Running tests');
+
+assert.ok(Respizer);
+var pathsThatShouldExist = ['1680/test.jpg', '1366/test.jpg', '1024@2x/test.jpg', '1024/test.jpg', '480@2x/test.jpg', '480/test.jpg', '320/test.jpg', '260/test.jpg', 'test.jpg'];
+
+for (var i=0; i<pathsThatShouldExist.length; i++){
+  var filePath = "./test/" + pathsThatShouldExist[i];
+  assert.ok(fs.existsSync(filePath));
+}
+
+console.log('Tests passed!');
+process.exit();
