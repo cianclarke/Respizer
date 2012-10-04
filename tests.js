9ac7af400405ce35ec7aef72b06c2e9e6610aca4
cianclarke <cian@cianclarke.com>
2012-10-04 21:03:52 +0100

    Needs some time to generate images

--- tests.js
+++ tests.js
@@ -5,12 +5,18 @@ fs = require('fs');
 console.log('Running tests');
 
 assert.ok(Respizer);
-var pathsThatShouldExist = ['1680/test.jpg', '1366/test.jpg', '1024@2x/test.jpg', '1024/test.jpg', '480@2x/test.jpg', '480/test.jpg', '320/test.jpg', '260/test.jpg', 'test.jpg'];
 
-for (var i=0; i<pathsThatShouldExist.length; i++){
-  var filePath = "./test/" + pathsThatShouldExist[i];
-  assert.ok(fs.existsSync(filePath));
-}
+// TODO: If Respizer throws events once done, listen for those instead of this horrible timeout..
+setTimeout(function(){
+  var pathsThatShouldExist = ['1680/test.jpg', '1366/test.jpg', '1024@2x/test.jpg', '1024/test.jpg', '480@2x/test.jpg', '480/test.jpg', '320/test.jpg', '260/test.jpg', 'test.jpg'];
 
-console.log('Tests passed!');
-process.exit();
+  for (var i=0; i<pathsThatShouldExist.length; i++){
+    var filePath = "./test/" + pathsThatShouldExist[i];
+    assert.ok(fs.existsSync(filePath));
+  }
+
+  console.log('Tests passed!');
+  process.exit();
+
+
+}, 2000);
